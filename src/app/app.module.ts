b753048af844ca932cfbdc382b5915effbad9881
arroniz08 <89236230+arroniz08@users.noreply.github.com>
2021-08-20 13:53:27 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { KanbanBoard } from './kanbanBoard/kanbanBoard.component';
+//Angular Router Module
+import { RouterModule, Router } from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    KanbanBoard
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterTestingModule,
+    RouterModule.forRoot([
+      { path:'', component: KanbanBoard }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
